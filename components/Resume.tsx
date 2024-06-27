8cdea75812ad40afd30a176337ee4f00133ff8bf
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-27 16:57:30 +0530

    Added New Section

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,11 +1,19 @@
-import Link from "next/link";
 import { BasicDetails } from "./Resume/BasicDetails";
+import { Experience } from "./Resume/Experience";
+import { Projects } from "./Resume/Projects";
+import { Skills } from "./Resume/Skills";
 
 export const Resume = () => {
   return (
     <div className=" bg-white shadow-md mx-16 h-[297mm] w-[220mm]  my-2 py-2 px-4">
         {/* BasicDetails */}
         <BasicDetails/>
+        {/* Projects */}
+        <Projects/>
+        {/* Experience */}
+        <Experience/>
+        {/* Skills */}
+        <Skills/>
     </div>
   );
 };
