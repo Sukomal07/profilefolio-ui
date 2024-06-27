55432b85723f2d3a857b462d32afa86468623f69
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-27 16:57:48 +0530

    Updated the Component

--- components/Resume/BasicDetails.tsx
+++ components/Resume/BasicDetails.tsx
@@ -4,15 +4,11 @@ export const BasicDetails = ()=>{
     <div className="text-center text-sm font-light">Software Developer</div>
     <div className="flex justify-center text-sm font-light">
         <div className=" border-r-2 border-gray-500 px-1">yashpatel.tech@gmail.com</div>
-        <div className="px-1">9408203060</div>
-    </div>
-    <div className="flex justify-center text-sm font-light">
+        <div className="px-1 border-r-2 border-gray-500">9408203060</div>
         <div  className="font-medium border-r-2 border-gray-500 px-1">
-           
            <a href="http://www.github.com/PatelYash7" target="_blank">
             Github
            </a>
-           
         </div>
         <div  className="font-medium px-1">
             <a href="http://www.linkedin.com/in/yash-patel-86666b1b9" target="_blank">
