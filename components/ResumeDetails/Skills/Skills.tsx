75415c0de2ecafef80ffd7f359b0ffabee120662
Sukomal07 <hypersukomal@gmail.com>
2024-06-29 11:00:46 +0530

    Add skills

--- components/ResumeDetails/Skills/Skills.tsx
+++ components/ResumeDetails/Skills/Skills.tsx
@@ -1,16 +1,36 @@
+"use client"
+
+import { InputWithLabel } from '@/components/InputWithLabel'
 import { ChevronDown, Database } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Skills() {
+    const [skills, setShowSkills] = useState(false)
     return (
-        <div className='flex justify-between items-center py-4 px-4 cursor-pointer'>
-            <div className='flex items-center gap-3'>
-                <div className='w-8 h-8 rounded-full flex justify-center items-center bg-slate-300'>
-                    <Database size={15} />
+        <div className='py-4 px-3 flex flex-col gap-4'>
+            <div className='flex justify-between items-center cursor-pointer' onClick={() => setShowSkills(!skills)}>
+                <div className='flex items-center gap-3'>
+                    <div className='w-8 h-8 rounded-full flex justify-center items-center bg-slate-300'>
+                        <Database size={15} />
+                    </div>
+                    <span className='text-slate-500 text-base'>Skills</span>
                 </div>
-                <span className='text-slate-500 text-base'>Skills</span>
+                <ChevronDown className={`text-slate-400 cursor-pointer transform transition-transform duration-300 ${skills ? 'rotate-180' : ''}`} size={20} />
             </div>
-            <ChevronDown className='text-slate-400 cursor-pointer' size={20} />
+            <div className={`transition-all duration-300 ease-in-out overflow-hidden ${skills ? 'block' : 'hidden'}`}>
+                <SkillsInputs />
+            </div>
+        </div>
+    )
+}
+
+export function SkillsInputs() {
+    return (
+        <div className='p-2 flex flex-col gap-3 items-center'>
+            <InputWithLabel label='Add languages' name='languages' type='text' placeholder='Java, javascript, TypeScript , Python' />
+            <InputWithLabel label='Add libraries / frameworks' name='libraries' type='text' placeholder='React.js , Next.js' />
+            <InputWithLabel label='Add tools / platforms' name='tools' type='text' placeholder='Vs code , Git' />
+            <InputWithLabel label='Add databases' name='database' type='text' placeholder='Mongodb , SQL , Postgress' />
         </div>
     )
 }
